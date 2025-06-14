c84f4dc7e7b2aa432cc6631b7d9ccea14c6225b4
raiden00pl <raiden00@railab.me>
2025-06-14 12:38:07 +0200

    boards/nucleo-c071rb: fix USART1 pins
    boards/nucleo-c071rb: fix USART1 pins
    
    Signed-off-by: raiden00pl <raiden00@railab.me>

--- boards/arm/stm32f0l0g0/nucleo-c071rb/include/board.h
+++ boards/arm/stm32f0l0g0/nucleo-c071rb/include/board.h
@@ -143,8 +143,8 @@
  *   USART1_TX - PB7
  */
 
-#define GPIO_USART1_RX      (GPIO_USART2_RX_2|GPIO_SPEED_HIGH)    /* PB6 */
-#define GPIO_USART1_TX      (GPIO_USART2_TX_2|GPIO_SPEED_HIGH)    /* PB7 */
+#define GPIO_USART1_RX      (GPIO_USART1_RX_2|GPIO_SPEED_HIGH)    /* PB6 */
+#define GPIO_USART1_TX      (GPIO_USART1_TX_2|GPIO_SPEED_HIGH)    /* PB7 */
 
 /* USART1 RS485_DIR - PA8 (arduino D7)
  * (compatible with RS485 Waveshare shield)
